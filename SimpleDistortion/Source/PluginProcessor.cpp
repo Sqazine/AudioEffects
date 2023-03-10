ce810be0371f38b09059a20547c30e7fa3b8f384
Sqazine <Sqazine@163.com>
2023-03-10 09:23:21 +0800

    add simple distorition

---
+++ SimpleDistortion/Source/PluginProcessor.cpp
@@ -0,0 +1,203 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+
+
+//==============================================================================
+SimpleDistortionAudioProcessor::SimpleDistortionAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+	addParameter(gain = new juce::AudioParameterFloat("gain", "Gain", 0.0f, 1.0f, 0.5f));
+	addParameter(invertPhase = new juce::AudioParameterBool("invertPhase", "Invert Phase", false));
+}
+
+SimpleDistortionAudioProcessor::~SimpleDistortionAudioProcessor()
+{
+}
+
+//==============================================================================
+const juce::String SimpleDistortionAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool SimpleDistortionAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool SimpleDistortionAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool SimpleDistortionAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double SimpleDistortionAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int SimpleDistortionAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int SimpleDistortionAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void SimpleDistortionAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String SimpleDistortionAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void SimpleDistortionAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+//==============================================================================
+void SimpleDistortionAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    processChain.reset();
+
+    juce::dsp::ProcessSpec spec;
+    spec.numChannels = 2;
+    spec.maximumBlockSize = samplesPerBlock;
+    spec.sampleRate = sampleRate;
+
+    processChain.prepare(spec);
+
+    auto& waveShaper = processChain.get<waveShaperIndex>();
+    waveShaper.functionToUse = [](float x)
+    {
+        return juce::jlimit(-0.1f, 0.1f, x);
+    };
+
+	auto phase = *invertPhase ? -1.0f : 1.0f;
+	previousGain = *gain;
+}
+
+void SimpleDistortionAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool SimpleDistortionAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    // Some plugin hosts, such as certain GarageBand versions, will only
+    // load plugins that support stereo bus layouts.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void SimpleDistortionAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+	auto phase = *invertPhase ? -1.0f : 1.0f;
+	auto currentGain = gain->get() * phase;
+	if (currentGain == previousGain)
+		buffer.applyGain(currentGain);
+	else
+	{
+		buffer.applyGainRamp(0, buffer.getNumSamples(), previousGain, currentGain);
+		previousGain = currentGain;
+	}
+
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+
+    juce::dsp::AudioBlock<float> block(buffer);
+    if (block.getNumChannels() > 2)
+        block = block.getSubsetChannelBlock(0, 2);
+
+	juce::dsp::ProcessContextReplacing<float> context(block);
+	processChain.process(context);
+}
+
+//==============================================================================
+bool SimpleDistortionAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* SimpleDistortionAudioProcessor::createEditor()
+{
+    return new juce::GenericAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void SimpleDistortionAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void SimpleDistortionAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new SimpleDistortionAudioProcessor();
+}
